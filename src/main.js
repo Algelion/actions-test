934425ef776252bddcc1a80f6cc605bdae9934db
Alexander Tkachenko <khalt.zommer@gmail.com>
2024-09-27 20:35:02 +0100

    Add test

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+require('dotenv').config();
+
+const API_SECRET = process.env.API_SECRET;
+const API_VAR = process.env.API_VAR;
+
+console.log('API_SECRET', API_SECRET);
+console.log('API_VAR', API_VAR);
