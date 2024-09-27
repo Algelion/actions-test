4018a7c490611846cbf6764728744121ed7e0be5
Alexander Tkachenko <khalt.zommer@gmail.com>
2024-09-27 20:38:17 +0100

    Raw api secret

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 const API_SECRET = process.env.API_SECRET;
 const API_VAR = process.env.API_VAR;
 
-console.log('API_SECRET', API_SECRET);
+console.log('RAW_API_SECRET', API_SECRET);
 console.log('API_VAR', API_VAR);
